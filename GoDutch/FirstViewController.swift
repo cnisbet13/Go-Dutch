16afb5606d5a4b3fc3da82098af8592bc6f89302
Calvin Nisbet <calvinnisbet@calvins-macbook-air.local>
2015-09-29 19:37:23 -0700

    Initial Commit

---
+++ GoDutch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoDutch
+//
+//  Created by Calvin Nisbet on 2015-09-29.
+//  Copyright © 2015 Lone Pine Creative. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
