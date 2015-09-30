a075daf64d76efaecf690865e0769fa36c35b79f
Calvin Nisbet <calvinnisbet@Calvins-MacBook-Air.local>
2015-09-30 16:59:20 -0700

    first commit

---
+++ GoDutch/NewViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewViewController.swift
+//  GoDutch
+//
+//  Created by Calvin Nisbet on 2015-09-30.
+//  Copyright © 2015 Lone Pine Creative. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
